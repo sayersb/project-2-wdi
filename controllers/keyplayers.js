6ca9f1289b90fdb996e5f31206e4ca92b2e1d8cc
sayersb <bradleysayers1@gmail.com>
2018-06-03 20:28:51 +0100

    keyplayer controllers added

--- controllers/keyplayers.js
+++ controllers/keyplayers.js
@@ -12,6 +12,55 @@ function keyPlayersIndex(req, res){
     });
 }
 
+function keyPlayersShow(req, res){
+  KeyPlayer
+    .findById(req.params.id)
+    .exec()
+    .then( keyplayer => {
+      res.render('keyplayers/show', {keyplayer});
+    });
+}
+
+function keyPlayersNew(req, res){
+  res.render('keyplayers/new');
+}
+
+function keyPlayersCreate(req, res){
+  KeyPlayer
+    .create(req.body)
+    .then((keyplayer) => {
+      return res.redirect(`keyplayers/${keyplayer._id}`);
+    });
+}
+
+function keyPlayersEdit(req, res){
+  KeyPlayer
+    .findById(req.params.id)
+    .exec()
+    .then( keyplayer => {
+      res.render('keyplayers/edit', {keyplayer});
+    });
+}
+
+function keyPlayersUpdate(req, res){
+  KeyPlayer
+    .findById(req.params.id)
+    .update(req.body)
+    .exec()
+    .then( keyplayer => {
+      return res.redirect(`/keyplayers/${req.params.id}`);
+    });
+}
+
+function keyPlayersDelete(req, res){
+  KeyPlayer
+    .findById(req.params.id)
+    .exec()
+    .then(keyplayer => {
+      keyplayer.remove();
+      return res.redirect('/keyplayers');
+    });
+}
 
 
 module.exports = {
